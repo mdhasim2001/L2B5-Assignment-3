6195509f5c0e2db29aa9ca5659b124cb2a360189
MD Hasim <mdhasim.nana.2001@gmail.com>
2025-06-21 23:22:25 +0600

    ok

--- src/app/controller/borrow.controller.ts
+++ src/app/controller/borrow.controller.ts
@@ -22,7 +22,27 @@ borrowBook.post("/", async (req: Request, res: Response) => {
 
 borrowBook.get("/", async (req: Request, res: Response) => {
   try {
-    const data = await Borrow.find().populate("book");
+    const data = await Borrow.aggregate([
+      {
+        $group: {
+          _id: "$book",
+          totalQuantity: { $sum: 1 },
+        },
+      },
+
+      {
+        $lookup: {
+          from: "books",
+          localField: "book",
+          foreignField: "_id",
+          as: "book",
+        },
+      },
+      // { $project: { title: 1, isbn: 1, totalQuantity: 1 } },
+    ]);
+
+    // const book = await Borrow.find().populate("book");
+
     res.status(200).send({
       success: true,
       data,
