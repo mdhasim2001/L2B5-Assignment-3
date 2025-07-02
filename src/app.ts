d037692012e875bdd7bd389b7ced2025a8c7e58a
MD Hasim <mdhasim.nana.2001@gmail.com>
2025-07-02 13:57:41 +0600

    cors install

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Application, Request, Response } from "express";
 import { bookRoutes } from "./app/controller/books.controller";
 import { borrowBook } from "./app/controller/borrow.controller";
+import cors from "cors";
 
 export const app: Application = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/books", bookRoutes);
